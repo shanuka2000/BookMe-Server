7b666f0f1cbbf609a4b02f361e41e6db3b6d48e8
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-12-05 23:36:10 +0530

    Middleware for error handling and authenticating request

---
+++ src/middleware/auth-handler-middleware.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { ACCESS_TOKEN_SECRET } from "../config/env-export-config.js";
+
+const JWT_SECRET = ACCESS_TOKEN_SECRET || "123";
+
+export interface AuthRequest extends Request {
+  user: {
+    id: string;
+    email: string;
+    role: "passenger" | "driver" | "admin";
+  };
+}
+
+const authenticateToken = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.cookies.access_token;
+
+    if (!token) {
+      res.status(401).json({ message: "Access denied. No token provided." });
+      return;
+    }
+
+    const decodedUser = jwt.verify(token, JWT_SECRET) as {
+      id: string;
+      email: string;
+      role: "passenger" | "driver" | "admin";
+    };
+
+    req.user = decodedUser;
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default authenticateToken;
---
+++ src/middleware/error-handler-middleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err.stack);
+  res
+    .status(500)
+    .json({ message: "Something went wrong. Please try again later." });
+};
