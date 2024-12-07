6da039d5ffdcc35ba6cf04d96db087e9444ccf31
Shanuka Peiris <115928935+shanuka2000@users.noreply.github.com>
2024-12-07 22:41:01 +0530

    minor validation for authentication middleware

--- src/middleware/auth-handler-middleware.ts
+++ src/middleware/auth-handler-middleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { ACCESS_TOKEN_SECRET } from "../config/env-export-config.js";
+import { ACCESS_TOKEN_SECRET, NODE_ENV } from "../config/env-export-config.js";
 
 const JWT_SECRET = ACCESS_TOKEN_SECRET || "123";
 
@@ -20,18 +20,20 @@ const authenticateToken = (
   try {
     const token = req.cookies.access_token;
 
-    if (!token) {
-      res.status(401).json({ message: "Access denied. No token provided." });
-      return;
+    if (NODE_ENV === "production") {
+      if (!token) {
+        res.status(401).json({ message: "Access denied. No token provided." });
+        return;
+      }
+
+      const decodedUser = jwt.verify(token, JWT_SECRET) as {
+        id: string;
+        email: string;
+        role: "passenger" | "driver" | "admin";
+      };
+
+      req.user = decodedUser;
     }
-
-    const decodedUser = jwt.verify(token, JWT_SECRET) as {
-      id: string;
-      email: string;
-      role: "passenger" | "driver" | "admin";
-    };
-
-    req.user = decodedUser;
     next();
   } catch (err) {
     next(err);
